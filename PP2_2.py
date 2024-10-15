5feb476cee0bc01c312ee58b69ceaf59089a8d30
Edward-Chen-ICD2O1-01 <edward.chen2@student.tdsb.on.ca>
2024-10-15 14:53:13 +0000

    done

--- PP2_2.py
+++ PP2_2.py
@@ -2,16 +2,37 @@
 
 def q1(): 
   #Write Assignment code here
-
-
+  num=float(input("Input an integer: "))
+  if num == 5:
+    print("The number is Five")
+  else:
+    print("The number is not Five")
+  
+  
 def q2(): 
   #Write Assignment code here
+  num= float(input("Input a number: "))
+  num1=0
+  if num > num1:
+    print("Postive")
+  else:
+    print("Negative")
 
 def q3(): 
   #Write Assignment code here
+  num= float(input("Input an integer: "))
+  if num %2==0:
+    print("Even")
+  else:
+    print("Odd")
 
 def q4(): 
   #Write Assignment code here
+  word= input('Type"Hello": ')
+  if word == "Hello":
+    print("The word is Hello")
+  else:
+    print("The word is not Hello")
 
 
 
