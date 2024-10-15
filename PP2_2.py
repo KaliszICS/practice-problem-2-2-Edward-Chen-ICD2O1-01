664f8c6420ec0838953a3286ae8315bbff1e24fc
Edward-Chen-ICD2O1-01 <edward.chen2@student.tdsb.on.ca>
2024-10-15 14:57:46 +0000

    finished

--- PP2_2.py
+++ PP2_2.py
@@ -2,7 +2,7 @@
 
 def q1(): 
   #Write Assignment code here
-  num=float(input("Input an integer: "))
+  num=int(input("Input an integer: "))
   if num == 5:
     print("The number is Five")
   else:
@@ -40,7 +40,7 @@ def q4():
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
-q3()
-q4()
+# q1()
+# q2()
+# q3()
+# q4()
